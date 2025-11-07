434efbf0d2df659f88d8c59f9153e1a218a5c217
fabuandras <fabuandras@gmail.com>
2025-11-07 10:38:16 +0100

    vmi

--- src/App.js
+++ src/App.js
@@ -1,21 +1,34 @@
 import './App.css';
+import { useState } from "react";
 import { konyvLista } from "./adat";
 import Konyvek from './components/Konyvek';
+import Kosar from './components/Kosar';
 
 function App() {
 
+  const [kosarLista, setKosarLista]=useState([])
   function kosarba(adat) {
-    console.log(adat);
+    const sl=[...kosarLista]
+    sl.push(konyvLista[adat])
+    setKosarLista([...sl])
+    console.log(kosarLista)
   }
 
   return (
     <div className="App">
+
       <header className="App-header">
         <h1>Könyvek</h1>
       </header>
+
+      <section className="kosar">
+        <Kosar lista={kosarLista} />
+      </section>
+
       <article>
         <Konyvek konyvLista={konyvLista} kosarbafv={kosarba} />
       </article>
+
     </div>
   );
 }
